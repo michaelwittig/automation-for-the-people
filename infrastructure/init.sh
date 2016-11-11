1a23c7530e3e035211728857d6272ce12b85a7aa
Michael Wittig <post@michaelwittig.info>
2016-11-11 15:27:03 +0100

    changed my mind about the application vrsion deployment. will do this in codepipeline iteself. not cfn

--- infrastructure/init.sh
+++ infrastructure/init.sh
@@ -6,5 +6,5 @@ read ARTIFACT_BUCKET_NAME
 echo "enter a GitHub OAuth token for your user's account. Generate one here: https://github.com/settings/tokens (only needs public_repo access)"
 read GITHUB_OAUTH_TOKEN
 
-# TODO add later aws --region eu-west-1 s3 mb "s3://${ARTIFACT_BUCKET_NAME}"
+aws --region eu-west-1 s3 mb "s3://${ARTIFACT_BUCKET_NAME}"
 aws --region eu-west-1 cloudformation create-stack --stack-name "aftp-pipeline" --template-body file://pipeline.yaml --parameters "ParameterKey=ArtifactsBucketName,ParameterValue=${ARTIFACT_BUCKET_NAME}" "ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_OAUTH_TOKEN}" --capabilities CAPABILITY_IAM
